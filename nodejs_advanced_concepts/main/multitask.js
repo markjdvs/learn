6f82bce0cecd6e3185eb1f6119fe60e0af51e6a0
Mark Davis <markjdvs@gmail.com>
2023-05-05 08:29:31 +0100

    setup

---
+++ nodejs_advanced_concepts/main/multitask.js
@@ -0,0 +1,20 @@
+const https = require('https');
+
+const start = Date.now();
+
+function doRequest() {
+    https
+        .request('https://www.google.com', res => {
+            res.on('data', () => {});
+            res.on('end', () => {
+                console.log(Date.now() - start);
+            })
+        })
+        .end();
+}
+
+function doHash() {
+    crypto.pbkdf2('a', 'b', 100000, 512, 'sha512', () => {
+        console.log('Hash:', Date.now() - start);
+    })
+}
